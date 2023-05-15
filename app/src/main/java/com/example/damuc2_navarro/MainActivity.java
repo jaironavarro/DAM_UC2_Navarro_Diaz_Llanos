f646ac595e0547d45e78f969ba8c37126850b1f7
Jairo Navarro <jairo_S27@outlook.com>
2023-05-15 16:52:49 -0500

    Parte 2 lista Daniel Diaz

--- app/src/main/java/com/example/damuc2_navarro/MainActivity.java
+++ app/src/main/java/com/example/damuc2_navarro/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.damuc2_navarro;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btn1 = findViewById(R.id.button_enviar);
+        btn1.setOnClickListener(view -> Toast.makeText(this, "Se envio sus datos", Toast.LENGTH_SHORT).show());
     }
 }
